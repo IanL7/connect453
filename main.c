edc3d6a73fc4c8105a33904a9ae9d6c00a46b19c
IanL7 <ilodes@wisc.edu>
2023-11-08 00:30:41 -0600

    work on state machine, draft of i2c

--- main.c
+++ main.c
@@ -21,13 +21,24 @@ cyhal_pwm_t servo_pwm_obj;
 cyhal_pwm_t game_state_led_b;
 cyhal_pwm_t game_state_led_r;
 cyhal_pwm_t game_state_led_g;
+cyhal_pwm_t winner_led_b;
+cyhal_pwm_t winner_led_r;
+cyhal_pwm_t winner_led_g;
 
 /*******************************************************************************
-* Static Global Variables (take away static when we have multiple files)
+* Global Variables (add static?)
 ******************************************************************************/
-static EventGroupHandle_t xConnectFourEventGroup;
-static QueueHandle_t xDistanceQueue;
-static QueueHandle_t xBoardQueue;
+EventGroupHandle_t xSMEventGroup;
+QueueHandle_t xDistanceQueue;
+QueueHandle_t xBoardQueue;
+
+// Message to request board state
+uint8_t rpi_i2c_request[3] = {1, 2, 3};
+
+// Board state responses
+// prev initialized to all empties for start of game
+uint8_t rpi_i2c_response_prev[BOARD_SIZE] = {EMPTY_PIECE};
+uint8_t rpi_i2c_response_curr[BOARD_SIZE];
 
 /*******************************************************************************
 * Task Handles
@@ -61,6 +72,12 @@ void rgb_on(cyhal_pwm_t *rgb_obj_r, cyhal_pwm_t *rgb_obj_g, cyhal_pwm_t *rgb_obj
         cyhal_pwm_stop(rgb_obj_r);
         cyhal_pwm_start(rgb_obj_b);
         cyhal_pwm_start(rgb_obj_g);
+        break;
+    
+    case RGB_BLUE:
+        cyhal_pwm_stop(rgb_obj_r);
+        cyhal_pwm_start(rgb_obj_b);
+        cyhal_pwm_stop(rgb_obj_g);
         break;
 
     default:
@@ -258,7 +275,7 @@ void pwm_init(int lin_hz, int lin_duty, int servo_hz, int servo_duty, int rgb_hz
     /////////////////////////////////////////////////////////////////
     /* Initialize PWM on the supplied pin and assign a new clock */
     rslt = cyhal_pwm_init(&lin_fore_pwm_obj, P5_6, NULL);
-    /* Set a duty cycle of 50% and frequency of 1Hz */
+    /* Set duty cycle */
     rslt = cyhal_pwm_set_duty_cycle(&lin_fore_pwm_obj, lin_duty, lin_hz);
     /* Stop the PWM output */
     rslt = cyhal_pwm_stop(&lin_fore_pwm_obj);
@@ -268,7 +285,7 @@ void pwm_init(int lin_hz, int lin_duty, int servo_hz, int servo_duty, int rgb_hz
     /////////////////////////////////////////////////////////////////
     /* Initialize PWM on the supplied pin and assign a new clock */
     rslt = cyhal_pwm_init(&lin_back_pwm_obj, P7_7, NULL);
-    /* Set a duty cycle of 50% and frequency of 1Hz */
+    /* Set duty cycle */
     rslt = cyhal_pwm_set_duty_cycle(&lin_back_pwm_obj, lin_duty, lin_hz);
     /* Stop the PWM output */
     rslt = cyhal_pwm_stop(&lin_back_pwm_obj);
@@ -278,7 +295,7 @@ void pwm_init(int lin_hz, int lin_duty, int servo_hz, int servo_duty, int rgb_hz
     /////////////////////////////////////////////////////////////////
     /* Initialize PWM on the supplied pin and assign a new clock */
     rslt = cyhal_pwm_init(&servo_pwm_obj, P6_3, NULL);
-    /* Set a duty cycle of 50% and frequency of 1Hz */
+    /* Set duty cycle */
     rslt = cyhal_pwm_set_duty_cycle(&servo_pwm_obj, servo_duty, servo_hz);
     /* Stop the PWM output */
     rslt = cyhal_pwm_stop(&servo_pwm_obj);
@@ -287,23 +304,47 @@ void pwm_init(int lin_hz, int lin_duty, int servo_hz, int servo_duty, int rgb_hz
     // RGB1 (game state led)
     /////////////////////////////////////////////////////////////////
     /* Initialize PWM on the supplied pin and assign a new clock */
-    rslt = cyhal_pwm_init(&game_state_led_b, P5_5, NULL);
-    /* Set a duty cycle of 50% and frequency of 1Hz */
+    rslt = cyhal_pwm_init(&game_state_led_b, PIN_GAME_STATE_LED_B, NULL);
+    /* Set duty cycle */
     rslt = cyhal_pwm_set_duty_cycle(&game_state_led_b, rgb_duty, rgb_hz);
     /* Stop the PWM output */
     rslt = cyhal_pwm_stop(&game_state_led_b);
 
     /* Initialize PWM on the supplied pin and assign a new clock */
-    rslt = cyhal_pwm_init(&game_state_led_r, P5_3, NULL);
-    /* Set a duty cycle of 50% and frequency of 1Hz */
+    rslt = cyhal_pwm_init(&game_state_led_r, PIN_GAME_STATE_LED_R, NULL);
+    /* Set duty cycle */
     rslt = cyhal_pwm_set_duty_cycle(&game_state_led_r, rgb_duty, rgb_hz);
     /* Stop the PWM output */
     rslt = cyhal_pwm_stop(&game_state_led_r);
 
     /* Initialize PWM on the supplied pin and assign a new clock */
-    rslt = cyhal_pwm_init(&game_state_led_g, P5_2, NULL);
-    /* Set a duty cycle of 50% and frequency of 1Hz */
+    rslt = cyhal_pwm_init(&game_state_led_g, PIN_GAME_STATE_LED_G, NULL);
+    /* Set duty cycle of 50% */
     rslt = cyhal_pwm_set_duty_cycle(&game_state_led_g, rgb_duty, rgb_hz);
+    /* Stop the PWM output */
+    rslt = cyhal_pwm_stop(&game_state_led_g);
+
+    /////////////////////////////////////////////////////////////////
+    // RGB2 (winner led)
+    /////////////////////////////////////////////////////////////////
+    /* Initialize PWM on the supplied pin and assign a new clock */
+    rslt = cyhal_pwm_init(&game_state_led_b, PIN_WINNER_LED_B, NULL);
+    /* Set duty cycle */
+    rslt = cyhal_pwm_set_duty_cycle(&winner_led_b, rgb_duty, rgb_hz);
+    /* Stop the PWM output */
+    rslt = cyhal_pwm_stop(&game_state_led_b);
+
+    /* Initialize PWM on the supplied pin and assign a new clock */
+    rslt = cyhal_pwm_init(&game_state_led_r, PIN_WINNER_LED_R, NULL);
+    /* Set duty cycle */
+    rslt = cyhal_pwm_set_duty_cycle(&winner_led_r, rgb_duty, rgb_hz);
+    /* Stop the PWM output */
+    rslt = cyhal_pwm_stop(&game_state_led_r);
+
+    /* Initialize PWM on the supplied pin and assign a new clock */
+    rslt = cyhal_pwm_init(&game_state_led_g, PIN_WINNER_LED_G, NULL);
+    /* Set duty cycle */
+    rslt = cyhal_pwm_set_duty_cycle(&winner_led_g, rgb_duty, rgb_hz);
     /* Stop the PWM output */
     rslt = cyhal_pwm_stop(&game_state_led_g);
 }
@@ -343,10 +384,114 @@ void mcu_startup_sound()
     cyhal_dac_free(&my_dac_obj);
 }
 
+void i2c_init()
+{
+    /* Declare variables */
+    cy_rslt_t rslt;
+
+    /* Define the I2C master configuration structure */
+    cyhal_i2c_cfg_t i2c_master_config = {CYHAL_I2C_MODE_MASTER, 0 /* address is not used for master mode */, I2C_MASTER_FREQUENCY};
+
+    /* Initialize I2C master, set the SDA and SCL pins and assign a new clock */
+    rslt = cyhal_i2c_init(&i2c_master_obj, PIN_I2C_SDA, PIN_I2C_SCL, NULL);
+
+    /* Configure the I2C resource to be master */
+    rslt = cyhal_i2c_configure(&i2c_master_obj, &i2c_master_config);
+}
+
+/* Defining master callback handler */
+void master_event_handler(void *callback_arg, cyhal_i2c_event_t event)
+{
+    BaseType_t xHigherPriorityTaskWoken, xResult;
+
+    /* xHigherPriorityTaskWoken must be initialised to pdFALSE. */
+    xHigherPriorityTaskWoken = pdFALSE;
+
+    /* To remove unused variable warning */
+    (void) callback_arg;
+    /* Check write complete event */
+    if (0UL != (CYHAL_I2C_SLAVE_WR_CMPLT_EVENT & event))
+    {
+        /* Perform the required functions */
+    }
+    /* Check read complete event */
+    if (0UL != (CYHAL_I2C_SLAVE_RD_CMPLT_EVENT & event))
+    {
+        xResult = xEventGroupSetBitsFromISR(
+                              xSMEventGroup,   /* The event group being updated. */
+                              EVENT_BOARD_RECEIVED_MASK, /* The bits being set. */
+                              &xHigherPriorityTaskWoken );
+
+        /* Was the message posted successfully? */
+        if( xResult != pdFAIL )
+        {
+            /* If xHigherPriorityTaskWoken is now set to pdTRUE then a context
+            switch should be requested.  The macro used is port specific and will
+            be either portYIELD_FROM_ISR() or portEND_SWITCHING_ISR() - refer to
+            the documentation page for the port being used. */
+            portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+        }
+    }
+    if (0UL != (CYHAL_I2C_SLAVE_ERR_EVENT & event))
+    {
+        /* In case of error abort transfer */
+        cyhal_i2c_abort_async(&i2c_master_obj);
+    }
+}
+
+void i2c_get_board_state()
+{
+    cy_rslt_t rslt;
+
+    // TODO: Put correct RPi slave address here
+    uint16_t I2C_SLAVE_ADDRESS = 0x08u;
+
+    cyhal_i2c_register_callback(&i2c_master_obj, (cyhal_i2c_event_callback_t) master_event_handler, NULL);
+    cyhal_i2c_enable_event(&i2c_master_obj, (cyhal_i2c_event_t)   \
+                                 (CYHAL_I2C_SLAVE_WR_CMPLT_EVENT  \
+                                | CYHAL_I2C_SLAVE_RD_CMPLT_EVENT  \
+                                | CYHAL_I2C_SLAVE_ERR_EVENT),    
+                                3u , true);
+
+    /* Initiate master write and read transfer using tx buffer and rx buffer respectively. */
+    cyhal_i2c_master_transfer_async(&i2c_master_obj, I2C_SLAVE_ADDRESS, rpi_i2c_request, 3, rpi_i2c_response_curr, BOARD_SIZE);
+}
+
+// Figure out state of board
+// Did someone win? Is there an error? etc.
+// Priority - error > winner > valid
+uint8_t process_board()
+{
+    // Check if error
+    int diff_count = 0;
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        // checking if p1 placed too many pieces
+        if ( rpi_i2c_response_prev[i] == EMPTY_PIECE && rpi_i2c_response_curr[i] == BLUE_PIECE ||
+             rpi_i2c_response_prev[i] == EMPTY_PIECE && rpi_i2c_response_curr[i] == YELLOW_PIECE )
+        {
+            diff_count++;
+        }
+        // p1 took out a piece
+        if ( rpi_i2c_response_prev[i] == BLUE_PIECE && rpi_i2c_response_curr[i] == EMPTY_PIECE ||
+             rpi_i2c_response_prev[i] == YELLOW_PIECE && rpi_i2c_response_curr[i] == EMPTY_PIECE )
+        {
+            return BOARD_ERROR;
+        }
+    }
+    if (diff_count > 1) 
+    {
+        return BOARD_ERROR;
+    }
+    // TODO: implement winner checks
+    return BOARD_VALID;
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 // FreeRTOS Tasks
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// TODO: Add serial outputs at key spots (ex. at different states)
 void task_state_manager(void *param)
 {
     static int STATE;
@@ -372,8 +517,8 @@ void task_state_manager(void *param)
                 cyhal_gpio_write(PIN_PLAYER1_LED, false);
 
                 xEventGroupWaitBits(
-                    xConnectFourEventGroup, 
-                    EVENT_PASS_TURN_MASK | EVENT_DROPPER_CLEAR_MASK,
+                    xSMEventGroup, 
+                    EVENT_PAUSE_MASK | EVENT_DROPPER_CLEAR_MASK,
                     pdTRUE,
                     pdTRUE,
                     portMAX_DELAY);
@@ -391,42 +536,57 @@ void task_state_manager(void *param)
                 cyhal_gpio_write(PIN_PLAYER2_LED, false);
                 cyhal_gpio_write(PIN_PLAYER1_LED, true);
 
+                // Send I2C signal to RPi to send over game state?
+
                 // Include mask here for error handling of player 1 move (doesn't put exactly one piece in)
                 // Should I be clearing all bits here?
                 for (;;) 
                 {
+                    // First wait for pass turn PB
                     xEventGroupWaitBits(
-                        xConnectFourEventGroup, 
-                        ALL_EVENTS_MASK,
-                        pdFALSE,
-                        pdFALSE,
+                        xSMEventGroup, 
+                        EVENT_PASS_TURN_MASK,
+                        pdTRUE,
+                        pdTRUE,
                         portMAX_DELAY);
-                    event = xEventGroupGetBits(xConnectFourEventGroup);
 
-                    if ( (event | EVENT_MOVE_ERROR) )
+                    // Send request to RPi for board state info via I2C
+                    i2c_get_board_state();
+
+                    // Wait for board state from RPi
+                    // State A - Player 1 
+                    xEventGroupWaitBits(
+                        xSMEventGroup, 
+                        EVENT_BOARD_RECEIVED_MASK,
+                        pdTRUE,
+                        pdTRUE,
+                        portMAX_DELAY);
+                    
+                    uint8_t board_state = process_board();
+
+                    if (board_state == BOARD_ERROR) 
                     {
-                        xEventGroupClearBits(xConnectFourEventGroup, ALL_EVENTS_MASK);
-                        STATE = STATE_MOVE_ERROR;
-                        break;
+                        rgb_on(&game_state_led_r, &game_state_led_g, &game_state_led_b, RGB_RED);
+                        // play error sound?
+                        continue;   // If there's an error, stay in P1 turn until they fix it
                     }
-                    else if ( (event | EVENT_PLAYER1_WIN) )
+                    else if (board_state == BOARD_WIN_P1)
                     {
-                        xEventGroupClearBits(xConnectFourEventGroup, ALL_EVENTS_MASK);
+                        rgb_on(&game_state_led_r, &game_state_led_g, &game_state_led_b, RGB_OFF);
                         STATE = STATE_PLAYER1_WIN;
                         break;
                     }
-                    else if ( (event | EVENT_PLAYER2_WIN) )
+                    else if (board_state == BOARD_WIN_P2)
                     {
-                        xEventGroupClearBits(xConnectFourEventGroup, ALL_EVENTS_MASK);
+                        rgb_on(&game_state_led_r, &game_state_led_g, &game_state_led_b, RGB_OFF);
                         STATE = STATE_PLAYER2_WIN;
                         break;
-                    }
-                    else if ( (event | EVENT_PASS_TURN_MASK) )
-                    {
-                        xEventGroupClearBits(xConnectFourEventGroup, ALL_EVENTS_MASK);
-                        STATE = STATE_P2_TURN;
-                        break;
-                    }
+                    } 
+                    // Board is valid
+                    // TODO: Send current board state over BLE to computer
+                    rgb_on(&game_state_led_r, &game_state_led_g, &game_state_led_b, RGB_GREEN);
+                    STATE = STATE_P2_TURN;
+                    break;
                 }
                 break;
 
@@ -434,6 +594,31 @@ void task_state_manager(void *param)
                 // Set LEDs
                 cyhal_gpio_write(PIN_PLAYER2_LED, false);
                 cyhal_gpio_write(PIN_PLAYER1_LED, true);
+
+                // TODO: Wait for P2 move from computer over BLE
+                xEventGroupWaitBits(
+                        xSMEventGroup, 
+                        EVENT_P2_MOVE_RECEIVED_MASK,
+                        pdTRUE,
+                        pdTRUE,
+                        portMAX_DELAY);
+
+            case STATE_PLAYER1_WIN:
+                vTaskSuspend(clear_dropper_handle);
+                // Set LEDs
+                // NOTE: PLAYER 1 ASSUMED TO BE YELLOW!
+                rgb_on(&winner_led_r, &winner_led_g, &winner_led_b, RGB_YELLOW);
+                cyhal_gpio_write(PIN_PLAYER2_LED, false);
+                cyhal_gpio_write(PIN_PLAYER1_LED, false);
+
+            case STATE_PLAYER2_WIN:
+                vTaskSuspend(clear_dropper_handle);
+                // Set LEDs
+                // NOTE: PLAYER 1 ASSUMED TO BE YELLOW!
+                rgb_on(&winner_led_r, &winner_led_g, &winner_led_b, RGB_BLUE);
+                cyhal_gpio_write(PIN_PLAYER2_LED, false);
+                cyhal_gpio_write(PIN_PLAYER1_LED, false);
+
 
         }
     }
@@ -465,7 +650,7 @@ void task_pole_pause_pb(void *param)
 
         if (curr_pb_state == PB_PRESSED && prev_pb_state == PB_NOT_PRESSED)
         {
-            xEventGroupSetBits(xConnectFourEventGroup, EVENT_PAUSE_MASK);
+            xEventGroupSetBits(xSMEventGroup, EVENT_PAUSE_MASK);
         }
         prev_pb_state = curr_pb_state;
     }
@@ -497,7 +682,7 @@ void task_pole_passturn_pb(void *param)
 
         if (curr_pb_state == PB_PRESSED && prev_pb_state == PB_NOT_PRESSED)
         {
-            xEventGroupSetBits(xConnectFourEventGroup, EVENT_PASS_TURN_MASK);
+            xEventGroupSetBits(xSMEventGroup, EVENT_PASS_TURN_MASK);
         }
         prev_pb_state = curr_pb_state;
     }
@@ -568,10 +753,14 @@ void task_clear_dropper(void *param)
             rslt = cyhal_pwm_stop(&lin_fore_pwm_obj);
             rslt = cyhal_pwm_stop(&lin_back_pwm_obj);
             
-            xEventGroupSetBits(xConnectFourEventGroup, EVENT_DROPPER_CLEAR_MASK);
+            xEventGroupSetBits(xSMEventGroup, EVENT_DROPPER_CLEAR_MASK);
         }
     }
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Entry point - init, power led, startup sound, start schedular
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(void)
 {
@@ -582,8 +771,12 @@ int main(void)
     CY_ASSERT(CY_RSLT_SUCCESS == rslt);
     mcu_all_leds_init();
     mcu_all_pbs_init();
+    i2c_init();
 
     __enable_irq();
+    
+    // Turn on ON-OFF LED
+    cyhal_gpio_write(PIN_ONOFF_LED, true);
 
     // Startup sound
     mcu_startup_sound();
@@ -591,7 +784,7 @@ int main(void)
     // Create inter-task messaging handles
     xDistanceQueue = xQueueCreate(1, sizeof( unsigned long )); // TODO: change type as necessary
     xBoardQueue = xQueueCreate(1, sizeof( unsigned long )); // TODO: change type as necessary
-    xConnectFourEventGroup = xEventGroupCreate();
+    xSMEventGroup = xEventGroupCreate();
 
     // Create tasks
     // TODO: Update priorites
--- main.h
+++ main.h
@@ -16,44 +16,67 @@
 //#include "drivers/io_expander.h"
 #include "drivers/LM75.h"
 
-#define BIT_0       (1 << 0)
-#define BIT_1       (1 << 1)
-#define BIT_2       (1 << 2)
-#define BIT_3       (1 << 3)
-#define BIT_4       (1 << 4)
-#define BIT_5       (1 << 5)
-#define BIT_6       (1 << 6)
-#define BIT_7       (1 << 7)
+// Board stuff
+#define BOARD_SIZE                  42          // board width * board height
+#define EMPTY_PIECE                 0
+#define BLUE_PIECE                  1
+#define YELLOW_PIECE                2
+#define BOARD_ERROR                 0
+#define BOARD_VALID                 1
+#define BOARD_WIN_P1                2
+#define BOARD_WIN_P2                3
 
-#define RGB_GREEN   1
-#define RGB_YELLOW  2
-#define RGB_RED     3
-#define RGB_OFF     4
+// Bits
+#define BIT_0                       (1 << 0)
+#define BIT_1                       (1 << 1)
+#define BIT_2                       (1 << 2)
+#define BIT_3                       (1 << 3)
+#define BIT_4                       (1 << 4)
+#define BIT_5                       (1 << 5)
+#define BIT_6                       (1 << 6)
+#define BIT_7                       (1 << 7)
 
-#define STATE_PAUSE         0
-#define STATE_INIT          1
-#define STATE_P1_TURN       2
-#define STATE_P2_TURN       3
-#define STATE_PLAYER1_WIN   4
-#define STATE_PLAYER2_WIN   5
-#define STATE_MOVE_ERROR    6
+// RGB LED states
+#define RGB_OFF                     0
+#define RGB_GREEN                   1
+#define RGB_YELLOW                  2
+#define RGB_RED                     3
+#define RGB_BLUE                    5
 
-#define PB_PRESSED      false
-#define PB_NOT_PRESSED  true
+// SM states
+#define STATE_PAUSE                 0
+#define STATE_INIT                  1
+#define STATE_P1_TURN               2
+#define STATE_P2_TURN               3
+#define STATE_PLAYER1_WIN           4
+#define STATE_PLAYER2_WIN           5
 
-#define ALL_EVENTS_MASK             0xFFFFFFFF
-#define EVENT_PASS_TURN_MASK        BIT_0
-#define EVENT_PAUSE_MASK            BIT_1 
-#define EVENT_DROPPER_CLEAR_MASK    BIT_2   
-#define EVENT_PLAYER1_WIN           BIT_3
-#define EVENT_PLAYER2_WIN           BIT_4
-#define EVENT_MOVE_ERROR            BIT_5
+// PB states
+#define PB_PRESSED                  false
+#define PB_NOT_PRESSED              true
 
-#define PIN_PASS_TURN_PB    P10_4
-#define PIN_PAUSE_PB        P10_2
-#define PIN_PLAYER1_LED     P10_1
-#define PIN_PLAYER2_LED     P9_4
-#define PIN_ONOFF_LED       P5_1
+// FreeRTOS Event Group masks
+#define ALL_EVENTS_MASK                     0xFFFFFFFF
+#define EVENT_PASS_TURN_MASK                BIT_0
+#define EVENT_PAUSE_MASK                    BIT_1 
+#define EVENT_DROPPER_CLEAR_MASK            BIT_2   
+#define EVENT_BOARD_RECEIVED_MASK           BIT_3
+#define EVENT_P2_MOVE_RECEIVED_MASK         BIT_4
+
+// Pins
+#define PIN_PASS_TURN_PB            P10_4
+#define PIN_PAUSE_PB                P10_2
+#define PIN_PLAYER1_LED             P10_1
+#define PIN_PLAYER2_LED             P9_4
+#define PIN_ONOFF_LED               P5_1
+#define PIN_GAME_STATE_LED_R        P5_3
+#define PIN_GAME_STATE_LED_G        P5_2
+#define PIN_GAME_STATE_LED_B        P5_5
+#define PIN_WINNER_LED_R            P12_6
+#define PIN_WINNER_LED_G            P12_7
+#define PIN_WINNER_LED_B            P10_2
+#define PIN_I2C_SCL                 P9_0
+#define PIN_I2C_SDA                 P9_1
 
 extern cyhal_pwm_t lin_fore_pwm_obj;
 extern cyhal_pwm_t lin_back_pwm_obj;
@@ -63,7 +86,5 @@ extern cyhal_pwm_t game_state_led_r;
 extern cyhal_pwm_t game_state_led_g;
 
 extern void rgb_on(cyhal_pwm_t *rgb_obj_r, cyhal_pwm_t *rgb_obj_g, cyhal_pwm_t *rgb_obj_b, int color);
-extern QueueHandle_t state_queue;
-
 
 #endif /* MAIN_H_ */
