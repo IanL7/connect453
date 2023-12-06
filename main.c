270816cd525f3235dcac2a13a8eed5133f881279
IanL7 <ilodes@wisc.edu>
2023-12-05 20:41:18 -0600

    git rid of freertos stuff

--- main.c
+++ main.c
@@ -5,13 +5,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "main.h"
-/* RTOS header files */
-#include <FreeRTOS.h>
-#include <FreeRTOSConfig.h>
-#include <task.h>
-#include <semphr.h>
-#include <event_groups.h>
-
 /*******************************************************************************
 * Global Variables
 ******************************************************************************/
